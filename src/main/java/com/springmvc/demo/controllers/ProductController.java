d49d05291ab019504ad04790f13b1b5ca119eec8
Levi <phamnguyentanphucgl@gmail.com>
2023-06-10 19:02:57 +0700

    remove unused import

--- src/main/java/com/springmvc/demo/controllers/ProductController.java
+++ src/main/java/com/springmvc/demo/controllers/ProductController.java
@@ -6,12 +6,10 @@ import com.springmvc.demo.repositories.CategoryRepository;
 import com.springmvc.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import java.util.Optional;
 import java.util.UUID;
 
 @Controller
